c45a07f09443b70dedf5a9951e4c77a8d866743d
Mcgeolypazun <taratula0504@gmail.com>
2024-07-18 21:16:13 +0900

    feat : naver 검색 api 사용

---
+++ restaurant/src/main/java/com/example/restaurant/wishlist/WishListEntity.java
@@ -0,0 +1,23 @@
+package com.example.restaurant.wishlist;
+
+import com.example.restaurant.db.MemoryDbEntity;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class WishListEntity extends MemoryDbEntity {
+
+    private String title;
+    private String category;
+    private String address;
+    private String roadAddress;
+    private String homePageLink;
+    private String imageLink;
+    private Boolean isVisit;
+    private int visitCount;
+    private LocalDateTime lastVisitDate;
+}
